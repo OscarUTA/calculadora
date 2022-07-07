10d30ae753c639d4c8df2724b5ade29ce9bffcee
OscarUTA <oscarflores_estudiante_@hotmail.com>
2022-07-07 06:59:46 -0500

    create page template

---
+++ calculadora/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Logo from './imagenes/utalogo.png';
+function App() {
+  return (
+    <div className='App'>
+      <div className='logo-center'>
+        <img 
+         src={Logo}
+         className='logo'
+         alt='logo de oscar uta' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
