f5988c9520b1f18bc7ae66220eda289549ae066b
OscarUTA <oscarflores_estudiante_@hotmail.com>
2022-07-07 12:55:47 -0500

    style: format was applied, style to appreciate on screen

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Logo from './imagenes/utalogo.png';
+import Boton from './componentes/Boton';
 function App() {
   return (
     <div className='App'>
@@ -9,6 +10,15 @@ function App() {
          src={Logo}
          className='logo'
          alt='logo de oscar uta' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <Boton>1</Boton>
+        <div className='fila'></div>
+        <Boton>+</Boton>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
       </div>
     </div>
   );
